dc654091a8061130f77c7c54dd56003efdc7b242
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2022-04-07 01:29:58 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./components/Main/Main";
-import Swap from "./components/Swap/Swap";
-import Facuet from "./components/Facuet/Facuet";
-import Reservoir from "./components/Reservoir/Reservoir";
+
 
 function Home() {
   return (
